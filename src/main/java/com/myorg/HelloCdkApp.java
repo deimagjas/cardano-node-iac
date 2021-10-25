f5ce2dc8e95edd69e3b0bc7adaa0bafbcf3d893a
deimagjas <deimagjas@gmail.com>
2021-10-24 22:04:39 -0500

    se cambia el nombre del stack

--- src/main/java/com/myorg/HelloCdkApp.java
+++ src/main/java/com/myorg/HelloCdkApp.java
@@ -10,7 +10,7 @@ public class HelloCdkApp {
     public static void main(final String[] args) {
         App app = new App();
 
-        new HelloCdkStack(app, "HelloCdkStack", StackProps.builder()
+        new HelloCdkStack(app, "CardanoCdkStack", StackProps.builder()
                 // If you don't specify 'env', this stack will be environment-agnostic.
                 // Account/Region-dependent features and context lookups will not work,
                 // but a single synthesized template can be deployed anywhere.
