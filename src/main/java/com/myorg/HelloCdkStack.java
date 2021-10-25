ecfbad159fdda1a8c9172b57bff9237fcbe13b84
deimagjas <deimagjas@gmail.com>
2021-10-24 23:05:00 -0500

    se agrega una subred pùblica

--- src/main/java/com/myorg/HelloCdkStack.java
+++ src/main/java/com/myorg/HelloCdkStack.java
@@ -4,6 +4,8 @@ import software.amazon.awscdk.core.Construct;
 import software.amazon.awscdk.core.RemovalPolicy;
 import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
+import software.amazon.awscdk.services.ec2.PublicSubnet;
+import software.amazon.awscdk.services.ec2.Subnet;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.s3.Bucket;
 
@@ -18,7 +20,12 @@ public class HelloCdkStack extends Stack {
         Vpc cardanoVpc = Vpc.Builder.create(this, "cardanoVpc")
                         .cidr("10.0.0.0/16")
                         .build();
-
+        Subnet cardanoPublicsubnet = PublicSubnet.Builder.create(this, "cardanoPublicSubnet")
+                                    .availabilityZone("us-east-1a")
+                                    .cidrBlock("10.0.1.0/24")
+                                    .mapPublicIpOnLaunch(true)
+                                    .vpcId("cardanoVpc")
+                                    .build();
     }
 
     private void cardanoArquitectureBucket () {
