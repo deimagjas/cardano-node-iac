78677c9f53ae98cf9c791fa0e0509fc3909c4409
deimagjas <deimagjas@gmail.com>
2021-10-25 22:26:47 -0500

    se refactoriza subred pública

--- src/main/java/com/myorg/HelloCdkStack.java
+++ src/main/java/com/myorg/HelloCdkStack.java
@@ -6,8 +6,13 @@ import software.amazon.awscdk.core.Stack;
 import software.amazon.awscdk.core.StackProps;
 import software.amazon.awscdk.services.ec2.PublicSubnet;
 import software.amazon.awscdk.services.ec2.Subnet;
+import software.amazon.awscdk.services.ec2.SubnetConfiguration;
 import software.amazon.awscdk.services.ec2.Vpc;
 import software.amazon.awscdk.services.s3.Bucket;
+import software.amazon.awscdk.services.ec2.SubnetType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class HelloCdkStack extends Stack {
     public HelloCdkStack(final Construct scope, final String id) {
@@ -17,15 +22,22 @@ public class HelloCdkStack extends Stack {
     public HelloCdkStack(final Construct scope, final String id, final StackProps props) {
         super(scope, id, props);
 
+        SubnetConfiguration subnetConfiguration = SubnetConfiguration.builder()
+                .name("cardanoPublicSubnet")
+                .subnetType(SubnetType.PUBLIC)
+                .cidrMask(28)
+                .build();
+
+        List<SubnetConfiguration> subnetList = new ArrayList<>();
+        subnetList.add(subnetConfiguration);
+
         Vpc cardanoVpc = Vpc.Builder.create(this, "cardanoVpc")
-                        .cidr("10.0.0.0/16")
-                        .build();
-        Subnet cardanoPublicsubnet = PublicSubnet.Builder.create(this, "cardanoPublicSubnet")
-                                    .availabilityZone("us-east-1a")
-                                    .cidrBlock("10.0.1.0/24")
-                                    .mapPublicIpOnLaunch(true)
-                                    .vpcId("cardanoVpc")
-                                    .build();
+                .cidr("10.0.0.0/26")
+                .enableDnsHostnames(false)
+                .maxAzs(4)
+                .subnetConfiguration(subnetList)
+                .vpnGateway(false)
+                .build();
     }
 
     private void cardanoArquitectureBucket () {
@@ -34,5 +46,9 @@ public class HelloCdkStack extends Stack {
                 .removalPolicy(RemovalPolicy.DESTROY)
                 .autoDeleteObjects(true)
                 .build();
+    }
+
+    private void configureCardanoNetwork () {
+
     }
 }
